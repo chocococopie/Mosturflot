a2dddd97788ddb9811527937c54ba8a12606c366
chocococopie <72577314+chocococopie@users.noreply.github.com>
2022-01-04 21:04:56 +0300

    Add files via upload

---
+++ js/plugins/chosen-seat.js
@@ -0,0 +1,7 @@
+// const collapseBtn = document.getElementById('choice-collapseOne');
+//
+// function selectDate(){
+//     if(this.classList.contains('show')){
+//         this.classList.remove('active');
+//     }
+// }
---
+++ js/plugins/import.js
@@ -0,0 +1,14 @@
+import './jquery';
+import './select2';
+import './bootstrap';
+import './swiper';
+import './bootstrap-table';
+import './air-datepicker';
+import './dropzone';
+import './tooltip';
+import './documents';
+import './select2-dropdown-below';
+import './modal-ticket';
+import './chosen-seat';
+import './navbar';
+import './search-field';
